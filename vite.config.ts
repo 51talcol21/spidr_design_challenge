83da3f90b6d4d8607f733af30910f170ac4c5d8d
25tal <72045932+51talcol21@users.noreply.github.com>
2025-07-15 10:18:28 -0700

    GH-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/spidr_design_challenge",
   plugins: [react(), tailwindcss()],
 })
